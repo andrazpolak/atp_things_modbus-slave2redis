b88a68162f1b190941f4b0b5854bea998645b2f4
andrazpolaktipteh <andrazpolaktipteh@gmail.com>
2021-08-20 14:57:46 +0200

    modbus client works

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 // imports
 const express = require('express');
-// let mustacheExpress = require('mustache-express');
-
-// const DeviceModbus = require('./src/modbus_master.js');
+let mustacheExpress = require('mustache-express');
+let ModbusRTU = require("modbus-serial");
+const DeviceModbus = require('./src/modbus_client');
 
 const config = require('config');// define process.env.NODE_CONFIG_DIR
 const module_cfg = JSON.parse(JSON.stringify(config.get("module")));
@@ -11,7 +11,6 @@ const devices_cfg = JSON.parse(JSON.stringify(config.get("devices")));
 console.log("Config [module]:", module_cfg);
 console.log("Config [devices]:", devices_cfg);
 
-// const things = require('./lib/things_atp/');
 
 const cors = require('cors');
 const appInfo = require('./package.json');
@@ -21,6 +20,56 @@ console.log(appInfo.name + ":", "Started. Env:", process.env.NODE_ENV);
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+let appRenderData = {
+    pageTitle: appInfo.name,
+    module: module_cfg
+}
+
+let devices = devices_cfg;
+console.log("Devices:", devices_cfg);
+let newDevice = new DeviceModbus(devices_cfg[0]);
+
+var vector = {
+    getInputRegister: async function (addr, unitID) {
+        return await newDevice.getInputRegister(addr, unitID);
+    },
+    getHoldingRegister: async function (addr, unitID) {
+        return await newDevice.getHoldingRegister(addr, unitID);
+    },
+    getCoil: async function (addr, unitID) {
+        return await newDevice.getCoil(addr, unitID);
+        // return true;
+
+    },
+    setRegister: async function (addr, value, unitID) {
+        return await newDevice.setRegister(addr, value, unitID);
+    },
+    setCoil: async function (addr, value, unitID) {
+        return await newDevice.setCoil(addr, value, unitID);
+    }
+};
+
+
+
+
+// HTTP 
+app.get('/', (req, res) => {
+    appRenderData.devices = devices.map(device => {
+        return {
+            identity: device.identity,
+            communication: device.communication,
+            data: {
+                inputs: newDevice.inputs(),
+                //  outputs: device.outputs(),
+                // status: device.status(),
+            }
+        };
+    });
+    res.render('enterName', appRenderData);
+});
 
 // REST api
 app.get('/version', (req, res) => {
@@ -45,7 +94,28 @@ app.get('/devices', (req, res) => {
     };
     res.json(response)
 })
+app.get('/coils', (req, res) => {
+    const response = {};
+    response.data = {
+        devices: devices_cfg
+    };
+    res.json(response)
+})
+app.get('/registers', (req, res) => {
+    const response = {};
+    response.data = {
+        devices: devices_cfg
+    };
+    res.json(response)
+})
 
+
+
+// Set Modbus TCP server
+const PORT_TCPIP = process.env.PORT_TCPIP || 502;
+console.log('ModbusTCP listening on modbus://0.0.0.0:' + PORT_TCPIP);
+var serverTCP = new ModbusRTU.ServerTCP(vector, { host: '0.0.0.0', port: PORT_TCPIP });
+// Set HTTP server
 const PORT_HTTP = process.env.PORT_HTTP || 11053;
 app.listen(PORT_HTTP, () => console.log(appInfo.name + ":", 'Listening on port ', PORT_HTTP))
 
---
+++ src/modbus_client.js
@@ -0,0 +1,168 @@
+"use strict";
+let ModbusRTU = require("modbus-serial");
+const { EventEmitter } = require('events');
+const atp_util = require('atp_util');
+
+
+function uniqueElementsOfArray(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+class DeviceModbus extends EventEmitter {
+    constructor(deviceModel) {
+        super();
+        this._connected = false;
+        this._error = false;
+        this._deviceModel = deviceModel;
+        this._modbus_client = new ModbusRTU();
+
+        this._identity = {
+            uuid: this._deviceModel.identity.uuid,
+            vendor: this._deviceModel.identity.vendor,
+            model: this._deviceModel.identity.model,
+            serial: this._deviceModel.identity.serial,
+            id_local: this._deviceModel.identity.id_local,
+            name: this._deviceModel.identity.name || "Name not defined"
+        }
+
+        this._communication = {
+            hostname: this._deviceModel.communication.hostname || "test",
+            type: this._deviceModel.communication.type || "modbusTCP",
+            port: this._deviceModel.communication.port || 502,
+            id: this._deviceModel.communication.id || 0,
+            read_interval_ms: this._deviceModel.communication.read_interval_ms || 200
+        }
+        this._localCoils = {};
+        this._localRegisters = {};
+        this._inputs = this.init_inputs(this._deviceModel.inputs);
+        // this._outputs = this.init_Outputs(this._deviceModel.outputs);
+
+        // //Get input registers
+        // this._inputs_registers = this.getInputRegisters(this._inputs);
+        // this._inputs_registers = this._inputs_registers.filter(uniqueElementsOfArray).sort(function (a, b) { return a - b });
+        // console.log("Input registers:", this._inputs_registers);
+        // this._inputs_registers_grouped_V2 = this.registersIntervals(this._inputs_registers);
+
+
+        // // Get input coils
+        // this._inputs_coils = this.getInputCoils(this._inputs);
+        // this._inputs_coils = this._inputs_coils.filter(uniqueElementsOfArray).sort(function (a, b) { return a - b });
+        // this._inputs_coils_grouped = this.registersIntervals(this._inputs_coils);
+
+    }
+
+    init_inputs(data) {
+        const inputs = data.map((input) => {
+            let measuringPoint = {
+                name: input.name,
+                register: input.register,
+                bit_offset: 0,
+                bit_length: 16,
+                ts: 0,
+                value: 0
+            };
+
+            if ('communication_type' in input)
+                measuringPoint.communication_type = input.communication_type;
+            if ('label' in input)
+                measuringPoint.label = input.label;
+            if ('bit_offset' in input)
+                measuringPoint.bit_offset = input.bit_offset;
+            if ('bit_length' in input)
+                measuringPoint.bit_length = input.bit_length;
+            if ('unit' in input)
+                measuringPoint.unit = input.unit;
+            if ('type' in input)
+                measuringPoint.type = input.type;
+            if ('value_type' in input)
+                measuringPoint.value_type = input.value_type;
+            if ('uuid' in input)
+                measuringPoint.uuid = input.uuid;
+            if ('convert_function' in input)
+                measuringPoint.convert_function = eval(input.convert_function);
+
+            if ('convert_keyvalue' in input && typeof input.convert_keyvalue === 'object')
+                measuringPoint.convert_keyvalue = input.convert_keyvalue;
+
+            // TODO: check if function is valid
+
+            return measuringPoint;
+        });
+
+        return inputs;
+    }
+    inputs() {
+        if (!atp_util.isArrayNotEmpty(this._inputs))
+            return [];
+
+        return this._inputs.map(el => {
+            let ret =
+            {
+                name: el.name,
+                ts: el.ts,
+                value: el.value
+            };
+
+            if ('label' in el)
+                ret.label = el.label;
+            if ('uuid' in el)
+                ret.uuid = el.uuid;
+            if ('unit' in el)
+                ret.unit = el.unit;
+            if ('type' in el)
+                ret.type = el.type;
+            // if ('value_type' in el)
+            //     ret.value_type = el.value_type;
+
+            return ret;
+        });
+    }
+    async setRegister(addr, value, unitID) {
+        console.log('set register', addr, value, unitID);
+        this._localRegisters[addr] = {
+            ts: Date.now(),
+            value: value
+        }
+        console.log('registers:', this._localRegisters);
+    }
+
+    async setCoil(addr, value, unitID) {
+        console.log('set coil', addr, value, unitID);
+        this._localCoils[addr] = {
+            ts: Date.now(),
+            value: value
+        }
+        console.log('Coils:', this._localCoils);
+    }
+
+    async getInputRegister(addr, unitID) {
+        if (this._localRegisters[addr] === undefined)
+            return 0;
+        // console.log('getRegisters1', addr, this._localRegisters[addr].value);
+        return this._localRegisters[addr].value;
+    };
+    async getHoldingRegister(addr, unitID) {
+        if (this._localRegisters[addr] === undefined)
+            return 0;
+        // console.log('getRegisters2', addr, this._localRegisters[addr].value);
+        return this._localRegisters[addr].value;
+    };
+    async getCoil(addr, unitID) {
+
+        if (this._localCoils[addr] === undefined)
+            return false;
+
+        // console.log('getCoil', addr, this._localCoils[addr].value);
+        return this._localCoils[addr].value;
+    };
+
+    info() {
+        console.log("Device info:");
+        console.log("Identity:", this._identity);
+        console.log("Communication:", this._communication);
+        console.log("Inputs:", this._inputs);
+        console.log("Outputs:", this._outputs);
+    }
+};
+
+module.exports = DeviceModbus;
